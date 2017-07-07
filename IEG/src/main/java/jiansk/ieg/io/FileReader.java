0ea90900bc2d5776a6c1e220ccdd9b1e5294afda
jiansk <jiansk20361@hundsun.com>
2017-07-07 10:16:50 +0800

    修复文件扫描空指针异常

--- IEG/src/main/java/jiansk/ieg/io/FileReader.java
+++ IEG/src/main/java/jiansk/ieg/io/FileReader.java
@@ -11,8 +11,12 @@ import java.util.List;
 public class FileReader {
 
     public static void main(String[] args) {
-        List<String> names = getInterfaceNames("/home/jiansk/IdeaProjects/IEG/src/main");
+        long start = System.currentTimeMillis();
+        List<String> names = getInterfaceNames("D:\\");
         System.out.println(names.toString());
+        long end = System.currentTimeMillis();
+        long cost = end - start;
+        System.out.println("耗时" + cost/1000 + 1 +"s");
     }
 
     public static List<String> getInterfaceNames(String filePath){
@@ -22,7 +26,7 @@ public class FileReader {
         int fileNum = 0;
         if(file.exists()){
             if(!file.isDirectory()){
-                System.out.println(filePath + "指定扫描路径应不正确");
+                System.out.println(filePath + "指定扫描路径不正确");
             }
             LinkedList<File> fileList = new LinkedList<File>();
             File[] files = file.listFiles();
@@ -35,16 +39,17 @@ public class FileReader {
                     names.add(file1.getName());
                 }
             }
-
             while (!fileList.isEmpty()) {
                 files = fileList.removeFirst().listFiles();
-                for (File file1 : files) {
-                    if (file1.isDirectory()) {
-                        fileList.add(file1);
-                    } else if (file1.getName().matches(regEx)) {
-                        System.out.println("文件:" + file1.getAbsolutePath());
-                        fileNum++;
-                        names.add(file1.getName());
+                if(files != null){
+                    for (File file1 : files) {
+                        if (file1 != null && file1.isDirectory()) {
+                            fileList.add(file1);
+                        } else if (file1 != null && file1.getName().matches(regEx)) {
+                            System.out.println("文件:" + file1.getAbsolutePath());
+                            fileNum++;
+                            names.add(file1.getName());
+                        }
                     }
                 }
             }
